c1d335dd84dc903369bc5ead6d8b3277f73cdaf3
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-25 17:53:11 -0400

    consolidated next and prev button components into one cycle component

---
+++ src/components/cycle-button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function CycleButton(props) {
+  const currentAnimal = props.pictures.find((picture) => {
+    return picture.id === props.value;
+  });
+
+  let upAnimal;
+  let downAnimal;
+  
+  if(props.name==='prev-button'){
+    upAnimal = props.pictures[props.pictures.indexOf(currentAnimal)-1];
+    downAnimal = props.pictures[props.pictures.length -1 ];
+  } else if (props.name==='next-button'){
+    upAnimal = props.pictures[props.pictures.indexOf(currentAnimal) +1];
+    downAnimal = props.pictures[0];
+  }
+
+  if (!upAnimal) {
+    return <button type="button" name={props.name} id={props.name} 
+      className={props.name} onClick={() => props.onClick(downAnimal.id)}>
+      Previous Animal</button>;
+  } else {
+    return <button type="button" name={props.name} id={props.name} 
+      className={props.name} onClick={() => props.onClick(upAnimal.id)}>
+      Previous Animal</button>;
+  }
+  
+}
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Dropdown from './dropdown';
 import Picture from './picture';
 import RandomButton from './random-button';
-import PrevButton from './prev-button';
-import NextButton from './next-button';
 import CSSButton from './cssbutton';
+import CycleButton from './cycle-button';
 
 export default class Gallery extends React.Component {
   constructor(props) {
@@ -40,9 +39,9 @@ export default class Gallery extends React.Component {
         <br></br>
         <Picture pictures={pictures} value={this.state.value} flipped={this.state.flipped} hue={this.state.hue}/>
         <br></br>
-        <PrevButton pictures={pictures} onClick={value => this.setState({value})} value={this.state.value}/>
+        <CycleButton pictures={pictures} name="prev-button" onClick={value => this.setState({value})} value={this.state.value} />
         <RandomButton pictures={pictures} onClick={value => this.setState({value})}/>
-        <NextButton pictures={pictures} onClick={value => this.setState({value})} value={this.state.value} />
+        <CycleButton pictures={pictures} name="next-button" onClick={value => this.setState({value})} value={this.state.value} />
         <br></br>
         <CSSButton name="flip-button" text="Vertical Flip" onClick={() => this.changeFlip()} />
         <CSSButton name="hue-button" text="Rotate Hue" onClick={() => this.rotateHue()} />
