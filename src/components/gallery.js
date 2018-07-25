70af03a4e030a3b5e9e79bbd3a8d23385b28be81
Filipp Gorbunov <Filippgorb@gmail.com>
2018-07-25 11:56:57 -0700

    Added prev and next buttons

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Dropdown from './dropdown';
 import Picture from './picture';
 import RandomButton from './random-button';
+import PrevButton from './prev-button';
+import NextButton from './next-button';
 
 export default class Gallery extends React.Component {
   constructor(props) {
@@ -20,7 +22,9 @@ export default class Gallery extends React.Component {
         <br></br>
         <Picture pictures={pictures} value={this.state.value}/>
         <br></br>
+        <PrevButton pictures={pictures} onClick={value => this.setState({value})} value={this.state.value}/>
         <RandomButton pictures={pictures} onClick={value => this.setState({value})}/>
+        <NextButton pictures={pictures} onClick={value => this.setState({value})} value={this.state.value} />
       </div>
 		)}
 }
---
+++ src/components/next-button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function NextButton(props) {
+  const currentAnimal = props.pictures.find((picture) => {
+    return picture.id === props.value
+  })
+  
+
+  const nextAnimal = props.pictures[props.pictures.indexOf(currentAnimal) +1]
+  const firstAnimal = props.pictures[0]
+
+  if (!nextAnimal) {
+    return <button type="button" name="next-button" id="next-button" newClass="next-button" onClick={e => props.onClick(firstAnimal.id)} >Next Animal</button>
+  } else {
+    return <button type="button" name="next-button" id="next-button" newClass="next-button" onClick={e => props.onClick(nextAnimal.id)} >Next Animal</button>
+  }
+  
+}
+
