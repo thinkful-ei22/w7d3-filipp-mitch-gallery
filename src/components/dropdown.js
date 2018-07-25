feb09a07d09fc9ccf792ff58df11aa8eb228b051
Filipp Gorbunov <Filippgorb@gmail.com>
2018-07-25 08:51:47 -0700

    Finished up static components

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function Dropdown() {
 	return (
-		<p>Dropdown</p>
+		<div>
+			<label for="dropdown">Choose an animal</label>
+			
+			<select newClass="dropdown" id="dropdown">
+				<option value="">--Please choose an option--</option>
+				<option value="dog">Dog</option>
+				<option value="cat">Cat</option>
+				<option value="horse">Horse</option>
+				<option value="shark">Shark</option>
+			</select>
+		</div>
 	)
 }
--- src/components/picture.js
+++ src/components/picture.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Picture() {
 	return (
-		<p>picture</p>
+		<img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Grey cat looking at the camera"/>
 	)
 }
